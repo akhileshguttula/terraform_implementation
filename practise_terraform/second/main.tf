25aac27396d5681bbf9f9657ef8401d5b98ff127
Ubuntu <ubuntu@ip-172-31-31-22.ec2.internal>
2026-09-02 06:15:06 +0000

    succesfully implemeneted ebs to the terraform_environment

---
+++ practise_terraform/second/main.tf
@@ -0,0 +1,38 @@
+
+resource "aws_instance" "instance_tf" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [var.security_group]
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+resource "aws_s3_bucket" "bucket_data" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = "Dev"
+  }
+}
+
+resource "aws_ebs_volume" "my_ebs_volume" {
+  availability_zone = aws_instance.instance_tf.availability_zone
+  size              = var.size
+
+  tags = {
+    Name = var.instance_name
+  }
+
+}
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.my_ebs_volume.id
+  instance_id = aws_instance.instance_tf.id
+}
+
